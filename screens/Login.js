c4ed55b30ef45cf7f999f835e21a2d74999c4adf
Lakisuru Semasinghe <semasinghels@gmail.com>
2020-10-07 23:42:57 +0530

    search added

--- screens/Login.js
+++ screens/Login.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import "react-native-gesture-handler";
 import firebase from "../firebaseDb";
+import firestore from "@react-native-firebase/firestore";
 
 import {
   StyleSheet,
@@ -12,39 +13,26 @@ import {
   Button,
 } from "react-native";
 
-// GoogleSignin.configure({
-//   webClientId: '253159788629-473o4b6oc3popct4f2b31tkkpf82kr64.apps.googleusercontent.com',
-// });
+function Push_data(pPhone, pPassword) {
+  // const dbRef = firebase.firestore().collection("users");
+  // dbRef
+  //   .add({
+  //     phone: pPhone,
+  //     password: pPassword,
+  //   })
+  //   .then((res) => {
+  //     console.log(res.id);
+  //   });
 
-// async function onGoogleButtonPress() {
-//   // Get the users ID token
-//   const { idToken } = await GoogleSignin.signIn();
-
-//   // Create a Google credential with the token
-//   const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
-//   // Sign-in the user with the credential
-//   return auth().signInWithCredential(googleCredential);
-// }
-
-// function GoogleSignIn() {
-//   return (
-//     <Button
-//       title="Google Sign-In"
-//       onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
-//     />
-//   );
-// }
-
-function Push_data(ffname, lname) {
-  const dbRef = firebase.firestore().collection("users");
-  dbRef
-    .add({
-      fname: ffname,
-      surename: lname,
+  firebase
+    .firestore()
+    .collection("users")
+    .doc(pPhone)
+    .set({
+      password: pPassword,
     })
-    .then((res) => {
-      console.log(res.id);
+    .then(() => {
+      console.log("User added!");
     });
 }
 
@@ -89,10 +77,11 @@ export default function Login({ route, navigation }) {
 
       <TouchableOpacity
         style={styles.loginBtn}
-        onPress={() => Push_data("Lakisuru", "Semasinghe")}
-        // onPress={() => navigation.navigate("Home", { lang: lang })}
+        //onPress={() => Push_data(phone, password)}
+
+        onPress={() => navigation.navigate("Home", { lang: lang })}
       >
-        <Text style={styles.loginText}>ADD DATA</Text>
+        <Text style={styles.loginText}>LOGIN</Text>
       </TouchableOpacity>
 
       <TouchableOpacity onPress={() => navigation.navigate("Account Recovery")}>
--- screens/Search.js
+++ screens/Search.js
@@ -1,30 +1,136 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import {
-  StyleSheet,
-  Text,
   View,
+  Text,
+  Button,
+  TextInput,
   Platform,
+  StyleSheet,
   KeyboardAvoidingView,
   ScrollView,
 } from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
 
-export default function Search({ Navigation }) {
-  return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS == "android" ? "padding" : "height"}
-      enabled
-    >
-      <View style={styles.container}>
-        <Text style={{ fontSize: 36 }}>SEARCH</Text>
-      </View>
-      <ScrollView></ScrollView>
-    </KeyboardAvoidingView>
-  );
+export default class SignUp extends React.Component {
+  state = {
+    keyword: "",
+    category: "",
+    district: "",
+  };
+  render() {
+    return (
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "android" ? "padding" : "height"}
+        enabled
+        style={styles.container}
+      >
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            padding: 40,
+          }}
+        >
+          <Text style={{ fontSize: 35, fontWeight: "bold" }}>Search</Text>
+        </View>
+        <ScrollView>
+          <Text>Keyword</Text>
+          <TextInput
+            style={styles.rectangle1}
+            placeholder="Keyword"
+            autoCapitalize="none"
+            placeholderTextColor="white"
+            onChangeText={(val) => this.onChangeText("keyword", val)}
+          />
+          <Text style={styles.category}>Category</Text>
+          <DropDownPicker
+            items={[
+              { label: "Harvest", value: "Harvest" },
+              { label: "Machinery", value: "Machinery" },
+            ]}
+            defaultIndex={0}
+            placeholder="Select Category"
+            containerStyle={{ width: 304, height: 43 }}
+            dropDownStyle={{ backgroundColor: "white" }}
+            placeholderStyle={{
+              fontWeight: "bold",
+              padding: 10,
+              color: "white",
+              borderRadius: 5,
+            }}
+            labelStyle={{
+              color: "white",
+              backgroundColor: "#1696f2",
+              padding: 10,
+              borderRadius: 30,
+              width: 300,
+            }}
+            onChangeItem={(item) => console.log(item.label, item.value)}
+          />
+
+          <Text style={styles.district}>District</Text>
+          <DropDownPicker
+            items={[
+              { label: "Anuradapura", value: "Anuradapura" },
+              { label: "Colombo", value: "Colombo" },
+            ]}
+            defaultIndex={0}
+            placeholder="Select District"
+            containerStyle={{ width: 304, height: 43 }}
+            dropDownStyle={{ backgroundColor: "white" }}
+            placeholderStyle={{
+              fontWeight: "bold",
+              padding: 10,
+              color: "white",
+              borderRadius: 5,
+            }}
+            labelStyle={{
+              color: "white",
+              backgroundColor: "#1696f2",
+              padding: 10,
+              borderRadius: 30,
+              width: 300,
+            }}
+            onChangeItem={(item) => console.log(item.label, item.value)}
+          />
+
+          <View
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              padding: 40,
+            }}
+          >
+            <Button title="Search" onPress={this.SignUp} />
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    );
+  }
 }
 
+// Style for "Search"
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: Platform.OS === "android" ? 35 : 0,
+  rectangle1: {
+    width: 304,
+    height: 43,
+    borderColor: "#707070",
+    borderStyle: "solid",
+    borderWidth: 1,
+    backgroundColor: "#1696f2",
+  },
+  keyword: {
+    width: 67,
+    height: 21,
+    color: "#050505",
+    fontFamily: "Times New Roman",
+    fontSize: 18,
+    fontWeight: "400",
+  },
+
+  background: {
+    width: 375,
+    height: 667,
+    backgroundColor: "#003f5c",
   },
 });
