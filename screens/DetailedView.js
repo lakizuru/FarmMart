801845d2b025796b1da681df2ef03729a3c3a07a
Lakisuru Semasinghe <semasinghels@gmail.com>
2020-10-31 13:08:03 +0530

    detailed view done

--- screens/DetailedView.js
+++ screens/DetailedView.js
@@ -1,13 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-//import React from "react";
-//import { StyleSheet, Text, View } from "react-native";
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import "react-native-gesture-handler";
 import firebase from "../firebaseDb";
 import {
   StyleSheet,
   Text,
   View,
+  Platform,
+  StatusBar
 } from "react-native";
 //import { Item } from "react-native-paper/lib/typescript/src/components/List/List";
 
@@ -15,7 +14,7 @@ export default function DetailedView({ Navigation, route }) {
   const { lang, doc } = route.params;
 
   const [pTitle, setPTitle] = useState();
-  const [pDiscription, setPDiscription] = useState();
+  const [pDescription, setPDescription] = useState();
   const [pLName, setPLName] = useState();
   const [pArea, setPArea] = useState();
   const [pCategory, setPCategory] = useState();
@@ -33,11 +32,11 @@ export default function DetailedView({ Navigation, route }) {
     const Post = firebase.firestore().collection("Posts").doc(doc);
 
   Post.get().then(function(post){
-    const {area, category, discription, district, price, qty, title, unit, user} = post.data();
+    const {area, category, description, district, price, qty, title, unit, user} = post.data();
 
     setPTitle(title);
     setUser(user);
-    setPDiscription(discription);
+    setPDescription(description);
     setPArea(area);
     setPCategory(category);
     setPDistrict(district);
@@ -59,27 +58,54 @@ export default function DetailedView({ Navigation, route }) {
 
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 28, fontWeight: 'bold'}}>{pTitle}</Text>
-      <Text style = {{fontSize: 36}}></Text>
-    <View>
-    <Text>{pCategory}</Text>
-     <Text>Price per {pUnit}: {pPrice}</Text>
+      <View style = {styles.card}>
+      <Text style={{fontSize: 40, fontWeight: 'bold'}}>{pTitle}</Text>
+
+      <Text style = {{fontSize: 0}}></Text>
+
+      <Text style = {{fontSize: 28, fontStyle: 'italic', color: 'grey'}}>{pCategory}</Text>
+
+      <Text style = {{fontSize: 24}}></Text>
+
+    <View style= {styles.subCard}>
+    <Text style = {{fontSize: 18, fontWeight: 'bold'}}>Product Description</Text>
+  <Text>{pDescription}</Text>
+    </View>
+
+    <Text style = {{fontSize: 24}}></Text>
+
+<View style = {styles.subCard}>
+<Text style = {{fontSize: 18, fontWeight: 'bold'}}>Product Information</Text>
+     <Text>Price per {pUnit}: LKR {pPrice}</Text>
      <Text>Qty: {pQty} {pUnit}</Text>
      <Text>Location: {pArea}, {pDistrict}</Text>
     </View>
      
-    <Text style = {{fontSize: 36}}></Text>
+    <Text style = {{fontSize: 24}}></Text>
 
-     <View>
-     <Text>Seller Information</Text>     
+     <View style= {styles.subCard}>
+     <Text style = {{fontSize: 18, fontWeight: 'bold'}}>Seller Information</Text>     
      <Text>Seller: {uFName} {uLName}</Text>
      <Text>From: {uArea}, {uDistrict}</Text>
     <Text>Phone: {user}</Text>
+      </View>
+      
+      <Text style = {{fontSize: 24}}></Text>
 
      </View>
-     
-    
+
+     <View style = {{width: '100%', alignItems:'center', borderRadius: 25, backgroundColor: 'red', borderColor: 'red', borderWidth: 15, width: '90%'}}>
+    {/* <Text style = {{fontWeight: 'bold', fontSize: '20', color: 'white'}}></Text>
+    <Text style = {{color: 'white'}}>Know who you're dealing with | Know what you're buying | Know what it will cost</Text> */}
+<Text style = {{color: 'white', fontSize: 24, fontWeight: 'bold'}}>⚠️ Be Careful! ⚠️</Text>
+<Text style = {{color: 'white',fontWeight: 'bold'}}>Know who you're dealing with.</Text>
+<Text style = {{color: 'white',fontWeight: 'bold'}}>Know what you're buying.</Text>
+<Text style = {{color: 'white',fontWeight: 'bold'}}>Know what it will cost.</Text>
+
     </View>
+
+    </View>
+    
   );
 
 }
@@ -87,10 +113,27 @@ export default function DetailedView({ Navigation, route }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "lightgreen",
-    alignItems: "baseline",
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    paddingBottom: 25
+  },
+  card:{
+    backgroundColor: 'lightgreen',
+    alignItems: "center",
     justifyContent: "flex-start",
-  },});
+    width: '90%',
+    borderRadius: 25,
+  },
+  subCard:{
+    borderRadius: 25,
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderWidth: 15,
+    width: '90%'
+  }
+});
   
 
 
